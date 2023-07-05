640cb74ba57ffdc1ab9d65cccdc252d080cd459b
Janaina Costa <88160110+janainaacosta@users.noreply.github.com>
2023-07-05 18:44:13 -0300

    Delete basico directory

--- basico/exemplos/aula04.py
+++
@@ -1,9 +0,0 @@
- # try | exception
-
-numero = input("informe o numero: ")
-
-try: 
-    numero_int = int(numero)
-    print("o dobro: ", numero_int * 2)
-except:
-    print("isso não é um número")
--- basico/exemplos/aula11.py
+++
@@ -1,6 +0,0 @@
-# range + for
-
-numeros = range(0,10,2) #inicio, fim, intervalo
-
-for valor in numeros:
-    print (valor)
