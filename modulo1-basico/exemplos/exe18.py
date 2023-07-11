4dd9ca959a1345b9b4c6c570aa5e6f69d1658fb8
Janaina Costa <88160110+janainaacosta@users.noreply.github.com>
2023-07-11 20:42:54 -0300

    Delete exe18.py

--- modulo1-basico/exemplos/exe18.py
+++
@@ -1,9 +0,0 @@
-'''
-introdução ao desempacotamento
-'''
-_, _, nome, *resto =  ['A', 'B', 'C', 'D', 'E']
-print(nome)
-print(' ')
-print(resto)
-
-#  underline representa uma variável que não vai ser usada
